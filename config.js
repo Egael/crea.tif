18311ab78f04871848d742fefbbee72c33cffc04
Egael <egaelvalle@gmail.com>
2026-01-22 14:58:20 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,31 @@
+const tarjeta = {
+    nombre: "Hilda Nuño",
+    descripcion: "<strong>Imprenta  tradicional y de vanguardia</strong><br>Con impresión digital calidad Xerox offset, serigrafía, sellos de goma y mucho, mucho más<br><strong>Siempre con atención personalizada y el mejor servicio.</strong>",
+    foto: "img/perfil.png",
+    portada: "img/portada.jpg",
+    telefono: "4925835868",
+    correo: "graphiczac@hotmail.com",
+
+  redes: [
+    {
+      nombre: "WhatsApp",
+      link: "https://wa.me/524925835868",
+      icono: "https://cdn-icons-png.flaticon.com/128/3670/3670051.png"
+    },
+    {
+      nombre: "Instagram",
+      link: "https://www.instagram.com/crea.tif_zac",
+      icono: "https://cdn-icons-png.flaticon.com/128/4494/4494488.png"
+    },
+    {
+      nombre: "Facebook",
+      link: "https://www.facebook.com/creatifzac",
+      icono: "https://cdn-icons-png.flaticon.com/128/5968/5968764.png"
+    },
+    {
+        nombre: "TikTok",
+        link: "https://www.tiktok.com/@crea.tif_zac",
+        icono: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWXXatvGdUncxQEmUwic8sz5sOGJCfT-ES2A&s"
+    }
+  ]
+};
